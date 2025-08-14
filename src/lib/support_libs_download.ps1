7c0c13884db162034ae7daee9f7e063418e42474
Louis Goodall <159187174+langoodall@users.noreply.github.com>
2025-08-14 13:22:21 -0400

    Update support_libs_download.ps1

--- src/lib/support_libs_download.ps1
+++ src/lib/support_libs_download.ps1
@@ -1,5 +1,5 @@
 # LANDIS-II support library GitHub URL
-$master = "https://github.com/LANDIS-II-Foundation/Support-Library-Dlls-v8/raw/main/"
+$master = "https://github.com/LANDIS-II-Foundation/Support-Library-Dlls-v7/raw/main/"
 
 
 #************************************************
